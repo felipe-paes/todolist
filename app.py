7b05b41812a4d7a8496040ef0d14278e65eec433
felipe-paes <felipe2013.paes@gmail.com>
2022-11-18 21:16:32 +0000

    metodo POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,8 +13,12 @@ def home():
     # templates / home.html
     return render_template('home.html', tasks=tasks)
 
-@app.route('/bye')
-def bye():
-    return 'Bye'
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    task = {'name': name, 'finished': False}
+    tasks.append(task)
+    return render_template('home.html', tasks=tasks)
+
 
 app.run(debug=True)
